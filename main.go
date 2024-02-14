bf792cd85b24ae59c29845c350ad8e20c386a178
Khammunh Thao <100525451+Khammunh@users.noreply.github.com>
2024-02-14 21:12:15 +0700

    data type golang

--- main.go
+++ main.go
@@ -3,6 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Golang")
+	// fmt.Println("Hello Golang")
+	/*fmt.Println("Khammunh")
 	fmt.Println("Khammunh")
+	fmt.Println("Khammunh")
+	fmt.Println("Khammunh")
+	fmt.Println("Khammunh")*/
+	fmt.Println("Khammunh")
+	//Manual Type declaration
+	var names string
+	names = "Khammunh manual type declaration"
+	fmt.Println(names)
+	//Type Inference
+	name2 := "Khammunh Type Inference"
+	fmt.Println(name2)
 }
