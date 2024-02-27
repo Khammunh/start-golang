e22ff3ce859a901e827697534b477b0bb705b0b5
Khammunh <khamlalago12345@gmail.com>
2024-02-27 10:15:37 +0700

    var,string

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Khamdsdfmcccing")
+	var name string = "test";
+	name2 := "test2";
+	var age int = 10 ;
+
+	fmt.Println(name)
+	fmt.Println("Khammunh",age, name2)
+	name2 = "testKhammunh"
+	
 }
--- seed/seed.go
+++ seed/seed.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	
-	"gorm.io/gorm"
 	"gorm.io/driver/mysql"
-
+	"gorm.io/gorm"
 )
 
 type User struct {
@@ -13,13 +11,13 @@ type User struct {
 	Fname    string
 	Lname    string
 	Username string
-	Avatar string
-	Email  string
+	Avatar   string
+	Email    string
 }
 
 func main() {
 	dsn := "root:123456789@tcp(127.0.0.1:3306)/go_orm?charset=utf8mb4&parseTime=True&loc=Local"
-  db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
@@ -27,20 +25,25 @@ func main() {
 	// Migrate the schema
 	db.AutoMigrate(&User{})
 
-	// Create
-	db.Create(&User{Code: "D42", Price: 100})
+	// Create a User
+	user := User{
+		Id:       "your_id_here",
+		Fname:    "John",
+		Lname:    "Doe",
+		Username: "johndoe",
+		Avatar:   "avatar_url_here",
+		Email:    "john@example.com",
+	}
+	db.Create(&user)
 
 	// Read
-	var product Product
-	db.First(&product, 1)                 // find product with integer primary key
-	db.First(&product, "code = ?", "D42") // find product with code D42
+	var fetchedUser User
+	db.First(&fetchedUser, 1)                        // find user with integer primary key
+	db.First(&fetchedUser, "id = ?", "your_id_here") // find user with specific id
 
-	// Update - update product's price to 200
-	db.Model(&product).Update("Price", 200)
-	// Update - update multiple fields
-	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
-	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+	// Update - update user's Fname to "Jane"
+	db.Model(&fetchedUser).Update("Fname", "Jane")
 
-	// Delete - delete product
-	db.Delete(&product, 1)
+	// Delete - delete user
+	db.Delete(&fetchedUser, 1)
 }
