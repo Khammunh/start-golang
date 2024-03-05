f94504bd7f3c7086aeb2e064a96f78bb5c107766
Khammunh <khamlalago12345@gmail.com>
2024-03-05 14:32:40 +0700

    'fiber'

--- main.go
+++ main.go
@@ -1,32 +1,15 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
-
-
-type Book struct {
-	ID  int `json': "id"`
-	Title string `json": "title"`
-	Author string `json": "author"`
-
-}
-var books  []Book
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-	books = append(books,Book{ID: 1, Title: "Mr John", Author: "Make"})
-	books = append(books,Book{ID: 2, Title: "Mr Son", Author: "Jecket 3"})
-    app.Get("/books",getBooks)	
-	app.Get("/books/:id",getBook)
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
 
-     app.Listen(":8080")
+	app.Listen(":8080")
 }
-
-func getBooks(c *fiber.Ctx) error {
-	return c.JSON(books)
-}
-func getBook(c *fiber.Ctx) error {
-	bookId := c.Params("id")
-	return c.SendString(bookId)
-}
-
